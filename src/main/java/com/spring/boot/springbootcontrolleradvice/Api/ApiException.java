6220d89d23c157f9eefb9e1c592027a10dd4f9b1
Abdullah Alwael <52009199+Abdullah-Alwael@users.noreply.github.com>
2025-08-16 15:43:21 +0300

    Delete src directory
    Wrong files uploaded

--- src/main/java/com/spring/boot/springbootcontrolleradvice/Api/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.spring.boot.springbootcontrolleradvice.Api;
-
-public class ApiException extends RuntimeException {
-
-    public ApiException(String message){
-        super(message);
-    }
-}
